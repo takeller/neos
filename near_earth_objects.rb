71f58f3f59321f438ac5274a20e9b7ece87fe983
Taylor Keller <takeller17@gmail.com>
2020-06-26 12:43:36 -0600

    Finish refactor

--- near_earth_objects.rb
+++ near_earth_objects.rb
@@ -6,32 +6,45 @@ Figaro.application = Figaro::Application.new(environment: 'production', path: Fi
 Figaro.load
 
 class NearEarthObjects
+
   def self.find_neos_by_date(date)
+    neo = NearEarthObjects.new
+    parsed_asteroids_data = neo.get_data(date)
+    neo.format_data(parsed_asteroids_data)
+  end
+
+  def get_data(date)
     conn = Faraday.new(
       url: 'https://api.nasa.gov',
       params: { start_date: date, api_key: ENV['nasa_api_key']}
     )
     asteroids_list_data = conn.get('/neo/rest/v1/feed')
 
-    parsed_asteroids_data = JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
+    JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
+  end
 
-    largest_astroid_diameter = parsed_asteroids_data.map do |astroid|
+  def format_data(data)
+    {
+      astroid_list: format_asteroid_list(data),
+      biggest_astroid: find_largest_asteroid_diameter(data),
+      total_number_of_astroids: data.count
+    }
+  end
+
+  def find_largest_asteroid_diameter(data)
+    data.map do |astroid|
       astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
     end.max { |a,b| a<=> b}
+  end
 
-    total_number_of_astroids = parsed_asteroids_data.count
-    formatted_asteroid_data = parsed_asteroids_data.map do |astroid|
+  def format_asteroid_list(data)
+    data.map do |astroid|
       {
         name: astroid[:name],
         diameter: "#{astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft",
         miss_distance: "#{astroid[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
       }
     end
-
-    {
-      astroid_list: formatted_asteroid_data,
-      biggest_astroid: largest_astroid_diameter,
-      total_number_of_astroids: total_number_of_astroids
-    }
   end
+
 end
